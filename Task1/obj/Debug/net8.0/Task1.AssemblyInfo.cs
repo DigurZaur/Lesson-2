b3d9b6df138d42e94d714f6a05bb24b198a61787
Zaurbek Digurov <digurzaur@yandex.ru>
2024-02-27 20:03:13 +0300

    Закомментил превью задания

--- Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb6f410df7f942adda6a68b0f755f546a0577894")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d30dc7cb2b0f6868248e77405ed334d368479b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
