007baf48935001ed724850f84e396e450061de1d
Zaurbek Digurov <digurzaur@yandex.ru>
2024-02-27 20:16:32 +0300

    Решаем задачу 2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Создать массив и заполнить его числами от 1 до N
+
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[N];
+int i = 0; 
+while(i < N)
+{
+    arr[i] = i + 1;
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}
