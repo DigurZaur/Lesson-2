2d30dc7cb2b0f6868248e77405ed334d368479b8
Zaurbek Digurov <digurzaur@yandex.ru>
2024-02-27 19:58:05 +0300

    Добавили задачу 1 в урок 2

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] arr = new int[5];
+arr[0] = 5;
+arr[1] = 6;
+arr[2] = 8;
+arr[3] = 2;
+arr[4] = 3;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+int[] arr2 = new int[5][1, 2, 3, 4, 5];
+
+int[] arr3 = { 1, 2, 3, 4, 5 };
+
+
